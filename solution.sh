75047e5ad3fd1bbbfb422c94ce3c15afe833d85c
princeochiss <princeochiss@gmail.com>
2022-11-29 01:38:30 -0500

    gitignore created

---
+++ solution.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#This is the very first hands working assignment.
+echo "forloop starts" 
+echo "running load testing"
+echo "Our systems can process a mINImum of 1 daily requests"
+for (( a=100;a>90;a-- ))
+do  
+echo $a  
+done
+echo "forloop ends"
